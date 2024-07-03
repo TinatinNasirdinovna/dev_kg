dfe2fd86f48d2eb3a2388f387e6560a9adea0bdb
asanbekovmyrzajan1@gmail.com <asanbekovmyrzajan1@gmail.com>
2024-07-03 09:37:15 +0600

    conflig chechtim

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Hero = () => {
+    return (
+        <div id='hero'>
+            <div className="hero">
+            <img src="https://devkg.com/images/events/e975ab09a375897590241aaec11d51fa.webp" alt="img" />
+            </div>
+        </div>
+    );
+};
+
+export default Hero;
