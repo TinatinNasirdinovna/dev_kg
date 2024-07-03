806a747515d41b05bb1861db5422a5dbe8e3d04c
asanbekovmyrzajan1@gmail.com <asanbekovmyrzajan1@gmail.com>
2024-07-03 10:11:36 +0600

    conflif ondodum ekichi jolu

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,8 @@
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -11,8 +10,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-       <App />
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
-
