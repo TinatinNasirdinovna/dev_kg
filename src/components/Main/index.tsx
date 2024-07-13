006c7718244f61c47e5dc78015799e098af7aceb
TinatinNasirdinovna <asankulovatinatin2002@gmail.com>
2024-07-13 10:19:21 +0600

    update

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -5,12 +5,13 @@ import Event from '../../pages/Event';
 import Video from '../../pages/Video';
 import Organizations from '../../pages/Organizations';
 import Community from '../../pages/Community';
+import Vacancies from '../Vacancies/Vacancies';
 
 const Main = () => {
     return (
         <div>
             <Routes>
-                <Route path='/' element={<Vacancy/>}/>
+                <Route path='/' element={<Vacancies/>}/>
                 <Route path='/events' element={<Event/>}/>
                 <Route path='/video' element={<Video/>}/>
                 <Route path='/organization' element={<Organizations/>}/>
