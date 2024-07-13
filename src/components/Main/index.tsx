2ce4e586c783e88a25bd5e14683662c22c326f18
burulsunZoldubekova <burulsunzoldubekova24@gmail.com>
2024-07-13 16:53:19 +0600

    detail vacancy buttu

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import Vacancy from '../../pages/Vacancy';
-import Event from '../../pages/Event';
-import Video from '../../pages/Video';
-import Organizations from '../../pages/Organizations';
-import Community from '../../pages/Community';
-import Vacancies from '../../pages/Vacancy';
-import DetailPage from '../../pages/DetailPage';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Vacancy from "../../pages/Vacancy";
+import Event from "../../pages/Event";
+import Video from "../../pages/Video";
+import Organizations from "../../pages/Organizations";
+import Community from "../../pages/Community";
+import Vacancies from "../../pages/Vacancy";
+import DetailPage from "../../pages/DetailPage";
+import DetailPosition from "../../pages/DetailPosition";
 
 const Main = () => {
-    return (
-        <div>
-            <Routes>
-                <Route path='/' element={<Vacancies/>}/>
-                <Route path='/events' element={<Event/>}/>
-                <Route path='/video' element={<Video/>}/>
-                <Route path='/organization' element={<Organizations/>}/>
-                <Route path='/community' element={<Community/>}/>
-                <Route path='/detail' element={<DetailPage/>}/>
-            </Routes>
-        </div>
-    );
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Vacancies />} />
+        <Route path="/events" element={<Event />} />
+        <Route path="/video" element={<Video />} />
+        <Route path="/organization" element={<Organizations />} />
+        <Route path="/community" element={<Community />} />
+        <Route path="/detail" element={<DetailPage />} />
+        <Route path="/detailPosition" element={<DetailPosition />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default Main;
--- src/pages/Vacancy/index.tsx
+++ src/pages/Vacancy/index.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Vacancies = () => {
+  const nav = useNavigate();
   return (
     <>
       <div id="vacancies">
@@ -14,41 +15,45 @@ const Vacancies = () => {
                 </Link>
               </div>
               <div className="jobs-list">
-                <a
-                  href="/ru/jobs/hr-menedzher-validon-agency-14277"
-                  className="link"
-                >
-                  <div className="jobs-item content">
-                    <div className="jobs-item-field icon">
-                      <div className="preview images">
-                        <picture>
-                          <img
-                            src="https://devkg.com/images/organizations/886e28d889f4c24ea73c66b80174110f.webp"
-                            alt=""
-                          />
-                        </picture>
-                      </div>
-                    </div>
-                    <div className="information">
-                      <div className="jobs-item-field company">
-                        <span className="label">Компания</span>
-                        Validon Agency
-                      </div>
-                      <div className="jobs-item-field position">
-                        <span className="label">Должность</span>
-                        Нr менеджер
-                      </div>
-                      <div className="jobs-item-field price">
-                        <span className="label">Оклад</span>
-                        От 60000 RUB в месяц
-                      </div>
-                      <div className="jobs-item-field type">
-                        <span className="label">Тип</span>
-                        Удаленная работа
-                      </div>
+                <div className="jobs-item content">
+                  <div className="jobs-item-field icon">
+                    <div className="preview images">
+                      <picture>
+                        <img
+                          src="https://devkg.com/images/organizations/886e28d889f4c24ea73c66b80174110f.webp"
+                          alt=""
+                        />
+                      </picture>
                     </div>
                   </div>
-                </a>
+                  <div className="information">
+                    <div
+                      className="jobs-item-field company"
+                      onClick={() => nav("/detailPosition")}
+                    >
+                      <span className="label">Компания</span>
+                      Validon Agency
+                    </div>
+                    <div className="jobs-item-field position" onClick={() => nav("/detailPosition")}>
+                      <span className="label">Должность</span>
+                      Нr менеджер
+                    </div>
+                    <div
+                      className="jobs-item-field price"
+                      onClick={() => nav("/detailPosition")}
+                    >
+                      <span className="label">Оклад</span>
+                      От 60000 RUB в месяц
+                    </div>
+                    <div
+                      className="jobs-item-field type"
+                      onClick={() => nav("/detailPosition")}
+                    >
+                      <span className="label">Тип</span>
+                      Удаленная работа
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
